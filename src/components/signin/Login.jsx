036bef823bacab825c912fecf5e88a38af74280d
Nde Dilan <nde.dilan@ictuniversity.edu.cm>
2024-06-08 22:43:47 +0100

    Adding A lot of changes

---
+++ src/components/signin/Login.jsx
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from "react";
+// import { getHome } from "../utils/ApiFunctions.js";
+import {
+  Anchor,
+  Button,
+  Container,
+  Form,
+  GhostButton,
+  Input,
+  LeftOverlayPanel,
+  Overlay,
+  OverlayContainer,
+  OverlayPanel,
+  Paragraph,
+  RightOverlayPanel,
+  SignInContainer,
+  SignUpContainer,
+  Title,
+} from "./Components.jsx";
+import "./Login.css";
+import { imageList } from "./images.js";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const [signIn, toggle] = React.useState(true);
+  const [newHome, setNewHome] = useState("");
+
+  const images = imageList;
+
+//   useEffect(() => {
+//     getHome().then((data) => {
+//       setNewHome(data);
+//     });
+//   }, []);
+
+  return (
+    <>
+      <Container>
+        <SignUpContainer signinIn={signIn}>
+          <Form>
+            <Title>Create Account</Title>
+            <Input type="text" placeholder="Full Name" />
+            <Input type="text" placeholder="UserName" />
+            <Input type="email" placeholder="Email" />
+            <Input type="password" placeholder="Password" />
+            <Button>Sign Up</Button>
+          </Form>
+        </SignUpContainer>
+
+        <SignInContainer signinIn={signIn}>
+          <Form>
+            <Title>Sign in</Title>
+            <Input type="text" placeholder="Username" />
+            <Input type="password" placeholder="Password" />
+            <Anchor href="#">Forgot your password?</Anchor>
+
+            <Button onClick={() => toggle(true)}>
+              <Link to={`/feed`}>Sigin In</Link>
+            </Button>
+          </Form>
+        </SignInContainer>
+
+        <OverlayContainer signinIn={signIn}>
+          <Overlay signinIn={signIn}>
+            <LeftOverlayPanel signinIn={signIn}>
+              <Title>Welcome Back!</Title>
+              <Paragraph>
+                To keep connected with us please login with your personal info
+              </Paragraph>
+              <GhostButton onClick={() => toggle(true)}>Sign In</GhostButton>
+            </LeftOverlayPanel>
+
+            <RightOverlayPanel signinIn={signIn}>
+              <Title>Hello, Friend!</Title>
+              <Paragraph>
+                Enter Your personal details and start journey with us
+              </Paragraph>
+              <GhostButton onClick={() => toggle(false)}>Sigin Up</GhostButton>
+            </RightOverlayPanel>
+          </Overlay>
+        </OverlayContainer>
+      </Container>
+
+      <div className="image-container">
+        {images.map((image, index) => (
+          <img
+            key={index}
+            className="image"
+            src={image}
+            alt={`Inclined Image ${index + 1}`}
+          />
+        ))}
+      </div>
+
+      {/* <div>
+        <h1>{newHome}</h1>
+      </div> */}
+    </>
+  );
+};
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { AuthContextProvider } from "./context/authContext";
+import { DarkModeContextProvider } from "./context/darkModeContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Login from "./components/signin/Login";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import Layout from "./pages/Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "feed",
+        element: <Home />,
+      },
+      {
+        path: "profile/:id",
+        element: <Profile />,
+      }
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <DarkModeContextProvider>
+      <AuthContextProvider>
+      <RouterProvider router={router} />
+      </AuthContextProvider>
+    </DarkModeContextProvider>
+    ,
   </React.StrictMode>
 );
