6d678918da77f6500b6d0b1de3cadef9580cbf7a
KING1ADB <kingadb181@gmail.com>
2024-06-09 06:26:37 -0700

    Deleted unused images

---
+++ src/data/images/images.jsx
@@ -0,0 +1,23 @@
+import image1 from './assets/image1.jpg';
+import image2 from './assets/image2.jpg';
+import image3 from './assets/image3.jpg';
+import image4 from './assets/image4.jpg';
+import image5 from './assets/image5.jpg';
+import image6 from './assets/image6.jpg';
+import image7 from './assets/image7.jpg';
+import image8 from './assets/image8.jpg';
+import image9 from './assets/image9.jpg';
+
+
+ const imageList = [
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+];
+
