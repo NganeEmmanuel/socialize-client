2064d99025bb392c8dea3f288ddb403c5c65d122
KING1ADB <kingadb181@gmail.com>
2024-06-09 23:51:12 -0700

    Modified Axios warnings

--- src/utils/APIFunctions.js
+++ src/utils/APIFunctions.js
@@ -6,25 +6,19 @@ const api = axios.create({
   baseURL,
 });
 
-export const signup = (data) => {
-  return api.post('/signup', data);
+const APIFunctions = {
+  signup: (data) => {
+    return api.post('/signup', data);
+  },
+  login: (data) => {
+    return api.post('/login', data);
+  },
+  getUsers: () => {
+    return api.get('/users');
+  },
+  createPost: (data) => {
+    return api.post('/posts', data);
+  },
 };
 
-export const login = (data) => {
-  return api.post('/login', data);
-};
-
-export const getUsers = () => {
-  return api.get('/users');
-};
-
-export const createPost = (data) => {
-  return api.post('/posts', data);
-};
-
-export default {
-  signup,
-  login,
-  getUsers,
-  createPost,
-};
+export default APIFunctions;
