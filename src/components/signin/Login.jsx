1cb45dd00837baba07d3b373db28593626787f45
Awasum Precious <97224559+PhotonX-AMP@users.noreply.github.com>
2024-06-10 12:32:36 +0200

    authprovider update

--- src/components/signin/Login.jsx
+++ src/components/signin/Login.jsx
@@ -21,18 +21,78 @@ import {
 } from "./Components.jsx";
 import "./Login.css";
 import { imageList } from "./images.js";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { useRef, useState, useEffect } from 'react';
+import useAuth from '../../hooks/useAuth';
+import axios from '../../api/axios';
+
+const LOGIN_URL = '/auth';
 
 const Login = () => {
   const [signIn, toggle] = React.useState(true);
 
+  const { setAuth } = useAuth();
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || "/";
+
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user, pwd])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post(LOGIN_URL,
+                JSON.stringify({ user, pwd }),
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                    withCredentials: true
+                }
+            );
+            console.log(JSON.stringify(response?.data));
+            //console.log(JSON.stringify(response));
+            const accessToken = response?.data?.accessToken;
+            const roles = response?.data?.roles;
+            setAuth({ user, pwd, roles, accessToken });
+            setUser('');
+            setPwd('');
+            navigate(from, { replace: true });
+        } catch (err) {
+            if (!err?.response) {
+                setErrMsg('No Server Response');
+            } else if (err.response?.status === 400) {
+                setErrMsg('Missing Username or Password');
+            } else if (err.response?.status === 401) {
+                setErrMsg('Unauthorized');
+            } else {
+                setErrMsg('Login Failed');
+            }
+            errRef.current.focus();
+        }
+    }
+
+    
   const images = imageList;
   return (
     <>
     <Navbar />
       <Container>
         <SignUpContainer signinIn={signIn}>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Title>Create Account</Title>
             <Input type="text" placeholder="Full Name" />
             <Input type="text" placeholder="UserName" />
@@ -43,10 +103,10 @@ const Login = () => {
         </SignUpContainer>
 
         <SignInContainer signinIn={signIn}>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Title>Sign in</Title>
-            <Input type="text" placeholder="Username" />
-            <Input type="password" placeholder="Password" />
+            <Input type="text" placeholder="Username" id="username" ref={userRef} autoComplete="off" onChange={(e) => setUser(e.target.value)}   value={user} required/>
+            <Input type="password" placeholder="Password" id="password"  onChange={(e) => setPwd(e.target.value)} value={pwd} required />
             <Anchor href="#">Forgot your password?</Anchor>
 
             <Button onClick={() => toggle(true)}>
