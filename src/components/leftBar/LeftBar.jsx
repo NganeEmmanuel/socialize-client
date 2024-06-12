a4cbcee9206497710e5d88a7e05e3666dd3af0af
KING1ADB <kingadb181@gmail.com>
2024-06-12 04:43:25 -0700

    Set the left bar compoents to buttons

--- src/components/leftBar/LeftBar.jsx
+++ src/components/leftBar/LeftBar.jsx
@@ -16,7 +16,6 @@ import { AuthContext } from "../../context/authContext";
 import { useContext } from "react";
 
 const LeftBar = () => {
-
   const { currentUser } = useContext(AuthContext);
 
   return (
@@ -24,72 +23,69 @@ const LeftBar = () => {
       <div className="container">
         <div className="menu">
           <div className="user">
-            <img
-              src={currentUser.profilePic}
-              alt=""
-            />
+            <img src={currentUser.profilePic} alt="" />
             <span>{currentUser.name}</span>
           </div>
-          <div className="item">
+          <button className="item">
             <img src={Friends} alt="" />
             <span>Friends</span>
-          </div>
-          <div className="item">
+          </button>
+          <button className="item">
             <img src={Groups} alt="" />
             <span>Groups</span>
-          </div>
-          <div className="item">
+          </button>
+          <button className="item">
             <img src={Market} alt="" />
             <span>Marketplace</span>
-          </div>
-          <div className="item">
+          </button>
+          <button className="item">
             <img src={Watch} alt="" />
             <span>Watch</span>
-          </div>
-          <div className="item">
+          </button>
+          <button className="item">
             <img src={Memories} alt="" />
             <span>Memories</span>
-          </div>
+          </button>
         </div>
         <hr />
         <div className="menu">
           <span>Your shortcuts</span>
-          <div className="item">
+          <button className="item">
             <img src={Events} alt="" />
             <span>Events</span>
-          </div>
-          <div className="item">
+          </button>
+          <button className="item">
             <img src={Gaming} alt="" />
             <span>Gaming</span>
-          </div>
-          <div className="item">
+          </button>
+          <button className="item">
             <img src={Gallery} alt="" />
             <span>Gallery</span>
-          </div>
-          <div className="item">
+          </button>
+          <button className="item">
             <img src={Videos} alt="" />
             <span>Videos</span>
-          </div>
-          <div className="item">
+          </button>
+          <button className="item">
             <img src={Messages} alt="" />
             <span>Messages</span>
-          </div>
+          </button>
         </div>
         <hr />
         <div className="menu">
           <span>Others</span>
-          <div className="item">
+          <button className="item">
             <img src={Fund} alt="" />
             <span>Fundraiser</span>
-          </div>
-          <div className="item">
+          </button>
+          <button className="item">
             <img src={Tutorials} alt="" />
             <span>Tutorials</span>
-          </div>
-          <div className="item">
+          </button>
+          <button className="item">
             <img src={Courses} alt="" />
             <span>Courses</span>
-          </div>
+          </button>
         </div>
       </div>
     </div>
