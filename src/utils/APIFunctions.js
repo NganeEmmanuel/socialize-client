0214a91f9e6cf2962258bebee7a2e26c6e5ca444
ChouatenY <chouatenyannrael@gmail.com>
2024-06-13 14:39:03 +0200

    pakage-lock npm install commit

--- src/utils/APIFunctions.js
+++ src/utils/APIFunctions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:8080';
+const baseURL = 'https://socialize-client-production.up.railway.app';
 
 const api = axios.create({
   baseURL,
