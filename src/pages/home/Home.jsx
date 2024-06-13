8cb249b70a46b64c2039f89476c11f8e64d7e335
Awasum Precious <97224559+PhotonX-AMP@users.noreply.github.com>
2024-06-13 14:05:10 +0200

    removed the story component from feed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,3 @@
-import Stories from "../../components/stories/Stories"
 import Posts from "../../components/posts/Posts"
 import Share from "../../components/share/Share"
 import "./home.scss"
@@ -6,7 +5,6 @@ import "./home.scss"
 const Home = () => {
   return (
     <div className="home">
-      <Stories/>
       <Share/>
       <Posts/>
     </div>
