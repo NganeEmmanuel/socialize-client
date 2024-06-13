03e29ba86b3e63e7fec539b36c402e1fa50b6b18
RositaChi <chirosita6@gmail.com>
2024-06-13 21:39:58 +0100

    .jsx for editpost

---
+++ src/pages/profile/EditPost.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './editpost.css';
+
+const EditPost = ({ post, onSave }) => {
+  const [caption, setCaption] = useState(post?.caption);
+  const [music, setMusic] = useState(post?.music);
+  const [privacy, setPrivacy] = useState('');
+
+
+  const handleSave = () => {
+    onSave({ caption, music });
+  };
+
+  return (
+    <div className="edit-post-container">
+      <div className="edit-post-header">
+        <div className="header-left">
+          <button className="back-button">
+            <i className="fas fa-arrow-left"></i>
+          </button>
+        </div>
+        <div className="header-right">
+          <button className="save-button" onClick={handleSave}>
+            Post
+          </button>
+          
