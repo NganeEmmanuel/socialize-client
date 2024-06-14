8b539ce4821857866b91a3bfb91ee35c0fe32727
RositaChi <chirosita6@gmail.com>
2024-06-14 09:59:46 +0100

    continuation of the logic for delete post

--- src/components/post/DeletePost.jsx
+++ src/components/post/DeletePost.jsx
@@ -13,3 +13,27 @@ const EditPost = ({ post, onSave, onDelete }) => {
   }
 }
 
+const onDeletePost = (postId) => {
+   
+    fetch(`/api/v1/post/delete/${postId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+       
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          
+          console.log(`Post with ID ${postId} has been deleted.`);
+          
+        } else {
+         
+          console.error(`Error deleting post with ID ${postId}: ${response.status}`);
+        }
+      })
+      .catch((error) => {
+        
+        console.error('Error deleting post:', error);
+      });
+  };
